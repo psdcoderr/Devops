c8dd449d348392e14be564859e60745390204dee
areesha <f219403@cfd.nu.edu.pk>
2025-02-06 12:18:22 +0500

    first commit setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
